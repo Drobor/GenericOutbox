f1913c26eb12b4d799e7f0d414e78bb2d426ad98
Drobor <m295-6d@ya.ru>
2023-08-03 19:51:57 +0300

    updated integration tests migrations

---
+++ Tests/IntegrationTests/Migrations/20230803165030_metadata.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IntegrationTests.Migrations
+{
+    /// <inheritdoc />
+    public partial class metadata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Metadata",
+                table: "OutboxEntity",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Metadata",
+                table: "OutboxEntity");
+        }
+    }
+}
